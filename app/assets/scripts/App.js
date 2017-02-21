d9cd1ee71fd4fe587766f29ad964f79789a02092
NICK VALLEE <nickvallee@gmail.com>
2017-02-21 14:42:24 -0500

    feat: knockout successfully imported and working!!! shiiiiiiiiiit

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,13 @@
+import ko from 'knockout';
+
+// This is a simple *view model* - JavaScript that defines the data and behavior of your UI
+function AppViewModel() {
+    this.firstName = ko.observable("Bert");
+    this.lastName = ko.observable("Bertington");
+
+    this.fullName = ko.computed(function() {
+    return this.firstName() + " " + this.lastName();}, this);
+}
+
+// Activates knockout.js
+ko.applyBindings(new AppViewModel());

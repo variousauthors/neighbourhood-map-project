56111c9d59ff6ae3208177818fa7acedb7594394
NICK VALLEE <nickvallee@gmail.com>
2017-02-23 09:23:33 -0500

    bug: filter is not a function

--- app/assets/scripts/modules/AppViewModel.js
+++ app/assets/scripts/modules/AppViewModel.js
@@ -9,6 +9,8 @@ var Location = function(data) {
 var AppViewModel = function() {
     var self = this;
 
+    this.filter = ko.observable();
+
     this.locationList = ko.observableArray([]);
 
     locations.forEach(function(locItem) {
@@ -19,6 +21,20 @@ var AppViewModel = function() {
 
 
 }
+
+AppViewModel.filteredItems = ko.computed(function() {
+      var filter = filter().toLowerCase();
+      if (!filter) {
+          return this.locationList();
+      } else {
+          return ko.utils.arrayFilter(this.locationList(), function(item) {
+              //return ko.utils.stringStartsWith(item.lastName().toLowerCase(), filter);
+              return (item.title().toLowerCase().indexOf(filter) > -1);
+          });
+      }
+
+  }, AppViewModel);
+
 
 
 ko.applyBindings(new AppViewModel());
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -16230,6 +16230,8 @@
 	var AppViewModel = function AppViewModel() {
 	    var self = this;
 
+	    this.filter = _knockout2.default.observable();
+
 	    this.locationList = _knockout2.default.observableArray([]);
 
 	    locations.forEach(function (locItem) {
@@ -16238,6 +16240,18 @@
 
 	    this.currentLocation = _knockout2.default.observable(this.locationList()[0]);
 	};
+
+	AppViewModel.filteredItems = _knockout2.default.computed(function () {
+	    var filter = filter().toLowerCase();
+	    if (!filter) {
+	        return this.locationList();
+	    } else {
+	        return _knockout2.default.utils.arrayFilter(this.locationList(), function (item) {
+	            //return ko.utils.stringStartsWith(item.lastName().toLowerCase(), filter);
+	            return item.title().toLowerCase().indexOf(filter) > -1;
+	        });
+	    }
+	}, AppViewModel);
 
 	_knockout2.default.applyBindings(new AppViewModel());
 
